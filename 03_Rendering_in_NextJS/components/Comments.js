ff79c6090e9ffa0beef885d73813b1e1e4dafa65
Prakash <pd14102003@gmail.com>
2025-08-06 05:37:16 +0530

    18_What is Hydration and Hydration error explained in page.js file in About page

--- 03_Rendering_in_NextJS/components/Comments.js
+++ 03_Rendering_in_NextJS/components/Comments.js
@@ -3,3 +3,14 @@ export default async function Comments() {
         <div>2k Comments</div>
     )
 }
+
+//Hydration is the process of adding interactivity to the pre rendered pages.
+
+//Hydration errors aree not shown in the production build.
+
+//Hydration error are shown when the HTML send by the server dosn't match with the HTML shown in the browser.
+// Means component rendered on the serveer and component rerendered on the client side not differnt.
+
+//Methods likee Math.random() and Date.now() give hydration error becuase when Math.random() execute on server it gives some other vale and when it execute on the client side it gives some other vale. so it gives Hydration error.
+
+// Hydration error are given so that developer can improvee the user experieence.
