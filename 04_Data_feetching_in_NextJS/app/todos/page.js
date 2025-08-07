74e03e69ca4bc85d83e0c085f8a54bb42a43c97c
Prakash <pd14102003@gmail.com>
2025-08-08 04:37:24 +0530

    20 How we can fetch data in the server component

---
+++ 04_Data_feetching_in_NextJS/app/todos/page.js
@@ -0,0 +1,24 @@
+const Todos = async () => {
+  const todos = await fetch("https://jsonplaceholder.typicode.com/todos?_limit=5");
+  const todosData = await todos.json();
+//   console.log(todosData);
+
+  return (
+    <>
+      <h1>Todos</h1>
+      <div className="todos-container">
+        {todosData.map(({ id, title, completed }) => (
+          <div className="post-card" key={id}>
+            <input type="checkbox" checked={completed} readOnly></input>
+            <p>{title}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Todos;
+
+
+//Note:- fetch of server side is different from client side , it gives us extra features like caching , prefetching , etc.You can read about them in detail in nextJS documentation
